63fac23de5056b2eabccdf2dad0f1dddeb935ce0
Marsel Brandao Junior <marseljrdev@gmail.com>
2023-05-04 17:30:04 -0300

    styleOverRides do botao

--- src/config/theme/defaultTheme.ts
+++ src/config/theme/defaultTheme.ts
@@ -1,14 +1,26 @@
-import {createTheme} from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
 
 const defaultTheme = createTheme({
-    palette: {
-        primary: {
-            main: '#e3340d',
-        },
-        secondary: {
-            main: '#ee6b2f'
-        },
+  palette: {
+    primary: {
+      main: '#e3340d'
+    },
+    secondary: {
+      main: '#ee6b2f'
     }
+  },
+  components: {
+    MuiButton: {
+        styleOverrides: {
+            root: {
+                backgroundColor: '#e3340d',
+                color: '#ffff',
+                margin: '8px'
+                
+            }
+        }
+    }
+  }
 });
 
 export default defaultTheme;
