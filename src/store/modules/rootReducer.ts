eff98d2b81c7eb3c3b43d567bb09d3138e120eb1
Marsel Brandao Junior <marseljrdev@gmail.com>
2023-05-04 17:32:36 -0300

    removendo reducer que nao utilizei

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import userReducer from './userSlice';
 import usersReducer from './usersSlice';
 import recadosReducer from './recadosSlice';
 
 export default combineReducers({
-  user: userReducer,
   users: usersReducer,
   recados: recadosReducer
 });
