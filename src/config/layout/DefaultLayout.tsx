f6edcb5182024945d8c88e7f4e29d2051898a1ba
Marsel Brandao Junior <marseljrdev@gmail.com>
2023-05-08 18:49:15 -0300

    Logica para remover o token e sair do sistema

--- src/config/layout/DefaultLayout.tsx
+++ src/config/layout/DefaultLayout.tsx
@@ -1,24 +1,31 @@
-import { AppBar, Box, Container, Grid, IconButton, Toolbar, Typography } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Box, Container, Grid, IconButton, Toolbar } from '@mui/material';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import LogoutIcon from '@mui/icons-material/Logout';
+import {  useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../../store/hooks';
+import { removeToken } from '../../store/modules/tokenSlice';
 
 interface DefaultLayoutProps {
   component: React.FC;
 }
 
 const DefaultLayout: React.FC<DefaultLayoutProps> = ({ component: Component }) => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const handleLogout = () => {
+    dispatch(removeToken());
+    navigate('/');
+  };
+
   return (
     <React.Fragment>
       <Box sx={{ flexGrow: 1, marginBottom: '15px' }}>
         <AppBar position="static">
           <Toolbar>
-            <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-              <MenuIcon />
+            <IconButton onClick={handleLogout} size="large" edge="end" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+              <LogoutIcon />
             </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
-              <Link to="/home">Pagina de recados</Link>
-            </Typography>
           </Toolbar>
         </AppBar>
       </Box>
